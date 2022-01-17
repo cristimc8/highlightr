b9dad44df683868f9e57521fd531a7fe89fe999b
Cristi <cristimc8@gmail.com>
2022-01-17 13:27:02 +0200

    adding rect chrome redux to popup and content

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,10 @@
 // If your extension doesn't need a background script, just leave this file empty
 
-messageInBackground();
+import {wrapStore} from "webext-redux";
+
+runBackgroundScripts()
 
 // This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
-export function messageInBackground() {
-  console.log('I can run your javascript like any other code in your project');
-  console.log('just do not forget, I cannot render anything !');
+export function runBackgroundScripts() {
+  wrapStore()
 }
