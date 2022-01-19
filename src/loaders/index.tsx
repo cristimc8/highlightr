db8c7374196435895ee628e5edfcecbd5f6b407f
Cristi <cristimc8@gmail.com>
2022-01-20 01:33:02 +0200

    button now injecting no matter what video

--- src/loaders/index.tsx
+++ src/loaders/index.tsx
@@ -3,23 +3,27 @@ import {Store} from "webext-redux";
 import {ports} from "../constants/ports";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
-import {BookmarkButton} from "../contentScript/components/BookmarkButton";
 import React, {useState} from "react";
-import {BurgerMenu} from "../contentScript/components/BurgerMenu";
 import {ContentApp} from "../contentScript/components/ContentApp";
-import {watchForLikeButtonsSpawn} from "../services/DOMObserver/DOMWatcher";
+import {readyToInject } from "../services/DOMObserver/DOMWatcher";
 
 export const setupClient = async ({clientApp}: { clientApp: IHighlightrClient }) => {
     console.log(`Highlightr ${clientApp.version.name}`)
 
-    watchForLikeButtonsSpawn(() => {
+    setInterval(() => {
+        injectWithChecking()
+    }, 500)
+}
+
+const injectWithChecking = () => {
+    if(readyToInject())
         injectBookmarkButton()
-    })
 }
 
 // Create an anchor and inject the button wrapped by thes
 // redux provider
 const injectBookmarkButton = () => {
+    console.log("Creating bookmark button")
     const anchor = document.createElement('div');
     anchor.style.display = "flex"
     anchor.style.alignItems = "center"
