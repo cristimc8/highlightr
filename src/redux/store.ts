3ecc8f60ed6044cf6f89fdd74c530e32481f4504
Cristi <cristimc8@gmail.com>
2022-01-19 13:55:50 +0200

    persistence layer preloading redux state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import {createStore} from "redux";
 import reducers from './reducers'
+import devToolsEnhancer from 'remote-redux-devtools';
 
-export const store = createStore(reducers)
+export const store = createStore(reducers, devToolsEnhancer())
